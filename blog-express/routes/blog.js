88fd64131fe1611c3360755339dad9095294b57d
ljy <ljy@ljydeMBP.hitronhub.home>
2022-03-27 15:09:52 -0400

    add blog express

---
+++ blog-express/routes/blog.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
+
+router.get('/list', (req, res, next) => {
+    let author = req.query.author || ''
+    const keyword = req.query.keyword || ''
+
+    if (req.query.isadmin) {
+        console.log('is admin')
+        // admin page
+        if (req.session.username == null) {
+            console.error('is admin, but no login')
+            // not signed in
+            res.json(
+                new ErrorModel('not signed in')
+            )
+            return
+        }
+        // force check my blog
+        author = req.session.username
+    }
+
+    const result = getList(author, keyword)
+    return result.then(listData => {
+        res.json(
+            new SuccessModel(listData)
+        )
+    })
+});
+
+router.get('/detail', (req, res, next) => {
+    const result = getDetail(req.query.id)
+    return result.then(data => {
+        res.json(
+            new SuccessModel(data)
+        )
+    })
+});
+
+router.post('/new', loginCheck, (req, res, next) => {
+    req.body.author = req.session.username
+    const result = newBlog(req.body)
+    return result.then(data => {
+        res.json(
+            new SuccessModel(data)
+        )
+    })
+})
+
+router.post('/update', loginCheck, (req, res, next) => {
+    const result = updateBlog(req.query.id, req.body)
+    return result.then(val => {
+        if (val) {
+            res.json(
+                new SuccessModel()
+            )
+        } else {
+            res.json(
+                new ErrorModel('update blog failed')
+            )
+        }
+    })
+})
+
+router.post('/del', loginCheck, (req, res, next) => {
+    const author = req.session.username
+    const result = delBlog(req.query.id, author)
+    return result.then(val => {
+        if (val) {
+            res.json(
+                new SuccessModel()
+            )
+        } else {
+            res.json(
+                new ErrorModel('delete blog failed')
+            )
+        }
+    })
+})
+
+module.exports = router;
